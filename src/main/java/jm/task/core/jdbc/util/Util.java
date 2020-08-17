edd1b0a5ffe821b67278f0eae12ae5f339479f16
Alexey Ashutov <i.ashutov@yandex.ru>
2020-08-17 12:46:17 +0300

    Данные для подключения записал, исправил возвращаемое значение

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static Connection getMySQLConnection() throws SQLException,
+                                                                 ClassNotFoundException {
+        String dbName = "preprojectDB";
+        String connectionProperty="?useSSL=false&serverTimezone=Europe/Moscow";
+        String hostName = "jdbc:mysql://127.0.0.1:3306/";
+        String userName = "root";
+        String password = "1234";
+        return DriverManager.getConnection(hostName+dbName+connectionProperty, userName, password);
+    }
+
+
 }
