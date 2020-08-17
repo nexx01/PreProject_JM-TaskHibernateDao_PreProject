f8b797b4fb7e3da263387d8588ef882bb5ddb827
Alexey Ashutov <i.ashutov@yandex.ru>
2020-08-17 15:55:51 +0300

    Написал логугу проги

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService=new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ivan","Ivanov",(byte)55);
+        userService.saveUser("Petr","Petrov",(byte)11);
+        userService.saveUser("Alex","Alexeev",(byte)34);
+        userService.saveUser("Alex","Ivanov",(byte)34);
+
+        for (User user : userService.getAllUsers()) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
