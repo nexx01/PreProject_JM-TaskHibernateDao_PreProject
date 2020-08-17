b4bca66517edef3169ee524dd15b4b7387d4b5d8
Alexey Ashutov <i.ashutov@yandex.ru>
2020-08-17 15:57:10 +0300

    CS

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,13 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
-
-import java.util.Iterator;
+/* Алгоритм работы приложения:
+ Создание таблицы User(ов)
+ Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
+ Получение всех User из базы и вывод в консоль ( должен быть переопределен toString в классе User)
+ Очистка таблицы User(ов)
+ Удаление таблицы*/
 
 public class Main {
     public static void main(String[] args) {
