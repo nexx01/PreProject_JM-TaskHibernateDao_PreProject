bc3d631364339b3c7c4594526ac772113d193210
Alexey Ashutov <i.ashutov@yandex.ru>
2020-08-19 07:31:02 +0300

    поправил CS

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -22,20 +23,51 @@ public class Util {
 
     private static SessionFactory hibernateSessionFactory;
 
+
+
+
+
+/*
     public static SessionFactory getHibernateSessionFactory() {
         if (hibernateSessionFactory == null) {
-            try {
+        try {
                 Configuration configuration = new Configuration().configure();
 
                 StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
                 hibernateSessionFactory = configuration.buildSessionFactory(builder.build());
 
+
             } catch (Exception e) {
-                System.out.println("Исключение!" + e);
+                System.out.println("Исключение******!" + e);
             }
         }
         return hibernateSessionFactory;
+    }*/
+
+    public static SessionFactory getHibernateSessionFactory() {
+        //Configuration cfg = new Configuration();
+        Configuration cfg = new Configuration().configure();
+       /* cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        cfg.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+        cfg.setProperty("hibernate.connection.url", "jdbc:mysql://127.0.0.1:3306/preprojectDB?useSSL=false&serverTimezone=Europe/Moscow");
+        cfg.setProperty("hibernate.connection.username", "root");
+        cfg.setProperty("hibernate.connection.password", "1234");
+        cfg.setProperty("hibernate.show_sql", "true");
+        cfg.setProperty("hibernate.format_sql", "true");*/
+       // cfg.addAnnotatedClass(User.class);
+        //cfg.addAnnotatedClass(Node.class);
+        //cfg.addAnnotatedClass(Fault.class);
+
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties());
+        hibernateSessionFactory = cfg.buildSessionFactory(builder.build());
+
+
+        return hibernateSessionFactory;
     }
+
+
+
+
 
 
     }
