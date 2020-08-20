aa37ea3a2355c2a9c8e9ff521e00c4c2f500e685
Alexey Ashutov <i.ashutov@yandex.ru>
2020-08-20 14:43:22 +0300

    CS

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,10 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.Arrays;
 /* Алгоритм работы приложения:
  Создание таблицы User(ов)
  Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
@@ -11,18 +14,12 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
-    }
-
-    public static void workWithJDBC(){
-        UserServiceImpl userService=new UserServiceImpl();
+        UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
-        userService.saveUser("Ivan","Ivanov",(byte)55);
-        userService.saveUser("Petr","Petrov",(byte)11);
-        userService.saveUser("Alex","Alexeev",(byte)34);
-        userService.saveUser("Alex","Ivanov",(byte)34);
+        userService.saveUser("Ivan", "Ivanov", (byte) 55);
+        userService.saveUser("Petr", "Petrov", (byte) 11);
+        userService.saveUser("Alex", "Alexeev", (byte) 34);
+        userService.saveUser("Alex", "Ivanov", (byte) 34);
 
         for (User user : userService.getAllUsers()) {
             System.out.println(user);
@@ -30,5 +27,6 @@ public class Main {
 
         userService.cleanUsersTable();
         userService.dropUsersTable();
+
     }
 }
