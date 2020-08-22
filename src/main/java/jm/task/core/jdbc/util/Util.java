5936d8f4906c3c8c2ccaf60e67b984043038134e
Alexey Ashutov <i.ashutov@yandex.ru>
2020-08-22 11:55:51 +0300

    Соединение с хибером без конфиг файла

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -10,67 +10,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
     public static Connection getMySQLConnection() throws SQLException,
                                                                  ClassNotFoundException {
         String dbName = "preprojectDB";
-        String connectionProperty="?useSSL=false&serverTimezone=Europe/Moscow";
+        String connectionProperty = "?useSSL=false&serverTimezone=Europe/Moscow";
         String hostName = "jdbc:mysql://127.0.0.1:3306/";
         String userName = "root";
         String password = "1234";
-        return DriverManager.getConnection(hostName+dbName+connectionProperty, userName, password);
+        return DriverManager.getConnection(hostName + dbName + connectionProperty, userName, password);
     }
 
-    private static SessionFactory hibernateSessionFactory;
-
-
-
-
-
-/*
     public static SessionFactory getHibernateSessionFactory() {
-        if (hibernateSessionFactory == null) {
-        try {
-                Configuration configuration = new Configuration().configure();
-
-                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-                hibernateSessionFactory = configuration.buildSessionFactory(builder.build());
-
-
-            } catch (Exception e) {
-                System.out.println("Исключение******!" + e);
-            }
-        }
-        return hibernateSessionFactory;
-    }*/
-
-    public static SessionFactory getHibernateSessionFactory() {
-        //Configuration cfg = new Configuration();
-        Configuration cfg = new Configuration().configure();
-       /* cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        Configuration cfg = new Configuration();
+        cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         cfg.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
         cfg.setProperty("hibernate.connection.url", "jdbc:mysql://127.0.0.1:3306/preprojectDB?useSSL=false&serverTimezone=Europe/Moscow");
         cfg.setProperty("hibernate.connection.username", "root");
         cfg.setProperty("hibernate.connection.password", "1234");
         cfg.setProperty("hibernate.show_sql", "true");
-        cfg.setProperty("hibernate.format_sql", "true");*/
-       // cfg.addAnnotatedClass(User.class);
-        //cfg.addAnnotatedClass(Node.class);
-        //cfg.addAnnotatedClass(Fault.class);
-
+        cfg.setProperty("hibernate.format_sql", "true");
         StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties());
-        hibernateSessionFactory = cfg.buildSessionFactory(builder.build());
-
-
+        SessionFactory hibernateSessionFactory = cfg.buildSessionFactory(builder.build());
+        cfg.addAnnotatedClass(User.class);
         return hibernateSessionFactory;
     }
-
-
-
-
-
-
-    }
+}
 
 
 
